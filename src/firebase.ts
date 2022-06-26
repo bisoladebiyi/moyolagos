e6a56f6ae6e61d53bec0b334a3a24dc917063ab8
abisola <abisola.ca@gmail.com>
2022-06-26 23:05:04 +0100

    add firebase keys to .env

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBz7ug0pl5OkOYopjljjAmVKBO4DdK8V5c",
-  authDomain: "moyolagos-95ef0.firebaseapp.com",
-  projectId: "moyolagos-95ef0",
-  storageBucket: "moyolagos-95ef0.appspot.com",
-  messagingSenderId: "963080748462",
-  appId: "1:963080748462:web:43515f317fb81b47f99c3f",
-  measurementId: "G-MV2VDRGMT2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
